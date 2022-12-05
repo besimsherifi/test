6bdf7794d230432b6efc9ac99753d2b2bd05c5c9
besimsherifi <sherifibesim@gmail.com>
2022-12-05 16:21:36 +0100

    commit

--- pages/[uri].js
+++ pages/[uri].js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Footer from '../components/Footer'
 import { getPostByUri } from '../lib/test-data';
+import { client } from '../lib/apollo'
+import { gql } from '@apollo/client';
 
 export default function SlugPage({ post }) {
 
@@ -12,14 +14,14 @@ export default function SlugPage({ post }) {
       </Head>
 
       <main>
-          <div className="siteHeader">
-            <h1 className="title">
-                {post.title}
-            </h1>
-            <p>✍️  &nbsp;&nbsp;{`${post.author.node.firstName} ${post.author.node.lastName}`} | 🗓️ &nbsp;&nbsp;{ new Date(post.date).toLocaleDateString() }</p>
-          </div>
-            <article dangerouslySetInnerHTML={{__html: post.content}}>   
-            </article>
+        <div className="siteHeader">
+          <h1 className="title">
+            {post.title}
+          </h1>
+          <p>✍️  &nbsp;&nbsp;{`${post.author.node.firstName} ${post.author.node.lastName}`} | 🗓️ &nbsp;&nbsp;{new Date(post.date).toLocaleDateString()}</p>
+        </div>
+        <article dangerouslySetInnerHTML={{ __html: post.content }}>
+        </article>
       </main>
 
       <Footer></Footer>
@@ -29,8 +31,30 @@ export default function SlugPage({ post }) {
 }
 
 
-export async function getStaticProps({ params }){
-  const response = await getPostByUri(params.uri)
+export async function getStaticProps({ params }) {
+  const GET_POST_BY_URI = gql`
+  query GetPostByURI($id: ID!) {
+    post(id: $id, idType: URI) {
+      title
+      content
+      date
+      uri
+      author {
+        node {
+          firstName
+          lastName
+        }
+      }
+    }
+  }
+  `
+
+  const response = await client.query({
+    query: GET_POST_BY_URI,
+    variables: {
+      id:params.uri
+    }
+  })
   const post = response?.data?.post
   return {
     props: {
@@ -39,10 +63,10 @@ export async function getStaticProps({ params }){
   }
 }
 
-export async function getStaticPaths(){
-    const paths = []
-    return {
-        paths,
-        fallback: 'blocking'
-    }
+export async function getStaticPaths() {
+  const paths = []
+  return {
+    paths,
+    fallback: 'blocking'
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Head from 'next/head';
 import Footer from '../components/Footer';
 import PostCard from '../components/PostCard';
 import { getAllPosts } from '../lib/test-data';
+import { client } from '../lib/apollo'
+import { gql } from '@apollo/client';
 
 
 export default function Home({ posts }) {
+  // console.log(posts);
   return (
     <div className="container">
       <Head>
@@ -37,9 +40,23 @@ export default function Home({ posts }) {
   )
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
 
-  const response = await getAllPosts()
+  const GET_POSTS = gql`
+  query GetAllPosts {
+    posts {
+      nodes {
+        title
+        content
+        uri
+        date
+      }
+    }
+  } 
+  `
+  const response = await client.query({
+    query: GET_POSTS
+  })
   const posts = response?.data?.posts?.nodes
   return {
     props: {
