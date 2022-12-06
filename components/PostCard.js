e077ed2a9032fd1eabbcaa3c1ce9a3699b02bc81
besimsherifi <sherifibesim@gmail.com>
2022-12-06 14:33:51 +0100

    deployment fixes

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function PostCard ({ post }){
     return (
-        <p className={"card"}>
+        <Link href={post.uri} className={"card"}>
             <a className="card">
                 <h3>{post.title} &rarr;</h3>
             </a>
-        </p>
+        </Link>
     )
 }
