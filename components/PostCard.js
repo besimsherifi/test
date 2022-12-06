8d699feef62f7baf69d75cae30d3d233b7f1725e
besimsherifi <sherifibesim@gmail.com>
2022-12-06 13:59:53 +0100

    fsdf

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function PostCard ({ post }){
     return (
-        <Link href={post.uri} className={"card"}>
+        <p className={"card"}>
             <a className="card">
                 <h3>{post.title} &rarr;</h3>
             </a>
-        </Link>
+        </p>
     )
 }
